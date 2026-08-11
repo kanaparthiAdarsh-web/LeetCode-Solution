6ac57677685c125802e542f51964e16d3617fcf4
Adarsh Kanaparthi <kanaparthiadarsh@gmail.com>
2026-08-11 22:09:13 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (23.06%) - LeetSync

--- 9-palindrome-number/palindrome-number.c
+++ 9-palindrome-number/palindrome-number.c
@@ -1,14 +1,5 @@
 bool isPalindrome(int x) {
-    long int temp,d,pal=0;
-    temp=x;
-    while(temp>0)
-    {
-        d=temp%10;
-        pal=pal*10+d;
-        temp/=10;
-    }
-    if(pal==x)
-        return true;
-    else
-        return false;    
+    long int pal=0;
+    for(long int temp=x;temp>0;pal=pal*10+(temp%10),temp/=10);
+    return((pal==x)? true:false);   
 }

94d58a7bd2ee89e5229af32d7e8cce048af5d36e
Adarsh Kanaparthi <kanaparthiadarsh@gmail.com>
2026-08-16 23:31:51 +0530

    Time: 2 ms (80.00%) | Memory: 18.1 MB (100.00%) - LeetSync

--- 2156-stone-game-ix/stone-game-ix.c
+++ 2156-stone-game-ix/stone-game-ix.c
@@ -1,15 +1,8 @@
 bool stoneGameIX(int* stones, int stonesSize) {
     int remin_0=0,remin_1=0,remin_2=0;
-    for(int i=0;i<stonesSize;i++)
-        if(stones[i]%3==0)
-            remin_0++;
-        else
-            if(stones[i]%3==1)
-                remin_1++;
-        else
-            remin_2++;
+    for(int i=0;i<stonesSize;(stones[i]%3==0) ? remin_0++ : (stones[i]%3==1)? remin_1++ : remin_2++,i++);
     if(remin_0%2)
-        return(remin_1>remin_2?remin_1-remin_2:remin_2-remin_1)>2;
+        return(remin_1 > remin_2 ? remin_1-remin_2 : remin_2-remin_1)>2;
     else
-        return remin_1>0 && remin_2>0;
+        return remin_1 > 0 && remin_2 > 0;
 }

a1089f3588189b32b7306e9809f0964acb1445f2
Adarsh Kanaparthi <kanaparthiadarsh@gmail.com>
2026-08-24 21:15:01 +0530

    Time: 108 ms (0.00%) | Memory: 16.7 MB (60.00%) - LeetSync

--- 2002-stone-game-viii/stone-game-viii.c
+++ 2002-stone-game-viii/stone-game-viii.c
@@ -1,14 +1,8 @@
-
 int stoneGameVIII(int* stones, int stonesSize) {
-    int* prefix = (int*)malloc(sizeof(int) * stonesSize);
-    prefix[0] = stones[0];
-    for (int i = 1; i < stonesSize;prefix[i] = prefix[i - 1] + stones[i],i++);
-    int dp = prefix[stonesSize - 1];
-
-    for (int i = stonesSize - 2,take_current=prefix[i]-dp; i >= 1;take_current=prefix[i]-dp, i--)
-        if (take_current > dp)
-            dp = take_current;
-
-    free(prefix);
+    for (int i = 1; i < stonesSize;stones[i] += stones[i - 1],i++);
+    int dp = stones[stonesSize - 1];
+    for (int i = stonesSize - 2,diff=stones[i]-dp; i >= 1;diff=stones[i]-dp, i--)
+        if (diff > dp)
+            dp = diff;
     return dp;
 }
